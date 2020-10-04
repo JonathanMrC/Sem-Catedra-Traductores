24852893a96935dcfb033b4e7bddcfd1b40ea347
Jonathan <jonathan.civrian@alumnos.udg.mx>
2020-10-03 20:35:12 -0500

    Agregue clase token y la funcion analizar lexico esta vacia

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,34 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtborigen = new System.Windows.Forms.TextBox();
             this.BtnAnalizar = new System.Windows.Forms.Button();
             this.dgv = new System.Windows.Forms.DataGridView();
-            this.Tokens = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.lexemas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtbox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
-            // txtborigen
-            // 
-            this.txtborigen.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtborigen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtborigen.Location = new System.Drawing.Point(13, 13);
-            this.txtborigen.MaximumSize = new System.Drawing.Size(345, 127);
-            this.txtborigen.Multiline = true;
-            this.txtborigen.Name = "txtborigen";
-            this.txtborigen.Size = new System.Drawing.Size(345, 127);
-            this.txtborigen.TabIndex = 0;
-            // 
             // BtnAnalizar
             // 
-            this.BtnAnalizar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.BtnAnalizar.Location = new System.Drawing.Point(364, 55);
+            this.BtnAnalizar.Location = new System.Drawing.Point(389, 91);
             this.BtnAnalizar.MaximumSize = new System.Drawing.Size(75, 47);
+            this.BtnAnalizar.MinimumSize = new System.Drawing.Size(75, 47);
             this.BtnAnalizar.Name = "BtnAnalizar";
             this.BtnAnalizar.Size = new System.Drawing.Size(75, 47);
             this.BtnAnalizar.TabIndex = 2;
@@ -67,43 +50,37 @@
             // 
             this.dgv.AllowUserToAddRows = false;
             this.dgv.AllowUserToDeleteRows = false;
-            this.dgv.AllowUserToResizeColumns = false;
-            this.dgv.AllowUserToResizeRows = false;
-            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Tokens,
-            this.lexemas});
-            this.dgv.Location = new System.Drawing.Point(12, 146);
-            this.dgv.MaximumSize = new System.Drawing.Size(424, 492);
+            this.dgv.Location = new System.Drawing.Point(13, 223);
+            this.dgv.MaximumSize = new System.Drawing.Size(1080, 215);
+            this.dgv.MinimumSize = new System.Drawing.Size(775, 215);
             this.dgv.Name = "dgv";
             this.dgv.ReadOnly = true;
-            this.dgv.Size = new System.Drawing.Size(424, 492);
+            this.dgv.Size = new System.Drawing.Size(775, 215);
             this.dgv.TabIndex = 3;
             // 
-            // Tokens
+            // txtbox
             // 
-            this.Tokens.HeaderText = "Tokens";
-            this.Tokens.Name = "Tokens";
-            this.Tokens.ReadOnly = true;
-            this.Tokens.Width = 68;
-            // 
-            // lexemas
-            // 
-            this.lexemas.HeaderText = "Palabras";
-            this.lexemas.Name = "lexemas";
-            this.lexemas.ReadOnly = true;
-            this.lexemas.Width = 73;
+            this.txtbox.Location = new System.Drawing.Point(13, 13);
+            this.txtbox.MaximumSize = new System.Drawing.Size(670, 204);
+            this.txtbox.MinimumSize = new System.Drawing.Size(370, 204);
+            this.txtbox.Multiline = true;
+            this.txtbox.Name = "txtbox";
+            this.txtbox.Size = new System.Drawing.Size(370, 204);
+            this.txtbox.TabIndex = 4;
             // 
             // Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtbox);
             this.Controls.Add(this.dgv);
             this.Controls.Add(this.BtnAnalizar);
-            this.Controls.Add(this.txtborigen);
+            this.MaximumSize = new System.Drawing.Size(1366, 768);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
             this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.ResumeLayout(false);
@@ -112,12 +89,9 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox txtborigen;
         private System.Windows.Forms.Button BtnAnalizar;
         private System.Windows.Forms.DataGridView dgv;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Tokens;
-        private System.Windows.Forms.DataGridViewTextBoxColumn lexemas;
+        private System.Windows.Forms.TextBox txtbox;
     }
 }
 
