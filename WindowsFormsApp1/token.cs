0b85d421503b98b77a519a26670ff0f89e881488
Jonathan <jonathan.civrian@alumnos.udg.mx>
2020-10-03 20:36:22 -0500

    Clase token agregada

---
+++ WindowsFormsApp1/token.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class token
+    {
+        string lexema, nombre;
+        int id;
+
+        public string Lexema { get => lexema; set => lexema = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Id { get => id; set => id = value; }
+    }
+}
