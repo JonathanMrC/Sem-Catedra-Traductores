0c41b691cc991b74a7422bdd28f18971eaeb44db
Jonathan <jonathan.civrian@alumnos.udg.mx>
2020-10-20 14:04:14 -0500

    Analizador Lexico terminado

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -52,7 +52,7 @@
             // 
             this.dgv.AllowUserToAddRows = false;
             this.dgv.AllowUserToDeleteRows = false;
-            this.dgv.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.dgv.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgv.Location = new System.Drawing.Point(13, 223);
@@ -63,12 +63,14 @@
             // 
             // txtbox
             // 
-            this.txtbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.txtbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtbox.Location = new System.Drawing.Point(13, 13);
             this.txtbox.Multiline = true;
             this.txtbox.Name = "txtbox";
+            this.txtbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.txtbox.Size = new System.Drawing.Size(670, 204);
             this.txtbox.TabIndex = 4;
             // 
@@ -84,7 +86,7 @@
             this.MinimumSize = new System.Drawing.Size(816, 489);
             this.Name = "Form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Analizador Lexico";
+            this.Text = "Analizador Sintactico";
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,26 +19,54 @@ namespace WindowsFormsApp1
         Dictionary<string, int> c_reservados;
         List<Point> reglas;
         List<List<int>> tabla;
-        bool valido = true;
-        
+        bool BALE;
+
         public Form()
         {
-            reglas = new List<Point>();
-            tabla = new List<List<int>>();
             c_reservados = new Dictionary<string, int> { { ";", 2 }, { ",", 3 }, { "(", 4 }, { ")", 5 }, { "{", 6 }, { "}", 7 } };
             reservadas = new Dictionary<string, int>{{ "if", 9 }, { "while", 10 }, { "return", 11 }, { "else", 12 } };
             tipodato = new HashSet<string> { "int", "float", "char", "void" };
+            tabla = new List<List<int>>();
             tokens = new Queue<token>();
+            reglas = new List<Point>();
             CargarDatos();
+            BALE = false;
             InitializeComponent();
             Formatodgv();
         }
+        void Formatodgv()
+        {
+            dgv.Columns.Clear();
+            dgv.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 12);
+            dgv.Columns.Add("", "");
+            dgv.Rows.Add("Lexema"); dgv.Rows.Add("Id");dgv.Rows.Add("Nombre");
+            dgv.Refresh();
+        }
+        int StringtoInt(string s)
+        {
+            int t = 0, i = 0;
+            bool n = false;
+            if (s[0] == '-')
+            {
+                n = true;
+                ++i;
+            }
+            for(;i < s.Count();++i)
+            {
+                t *= 10;
+                t += s[i] - '0';
+            }
+            return n? -t : t;
+        }
+       
+        #region Cargar Tabla de reglas y transiciones
         void CargarDatos()//Leer reglas y tabla
         {
             string dir_reglas = "GR2slrRulesId.txt", dir_tabla = "GR2slrTablebien.txt", linea;
             if (!File.Exists(dir_reglas) || !File.Exists(dir_tabla))
             {
-                MessageBox.Show("No se encuentra el archivo "+dir_reglas+" o "+dir_tabla +"\r\nColocar estos archivos en la misma carpeta del ejecutable");
+                MessageBox.Show("No se encuentra el archivo "+dir_reglas+" o "+dir_tabla 
+                                +"\r\nColocar estos archivos en la misma carpeta del ejecutable");
                 return;
             }
             StreamReader archivo = new StreamReader(dir_reglas);
@@ -57,50 +85,29 @@ namespace WindowsFormsApp1
             {
                 string[] columnas = linea.Split('\t');
                 List<int> temp = new List<int>();
-                foreach (string s in columnas) temp.Add(StringtoInt(s));
+                foreach(string s in columnas) temp.Add(StringtoInt(s));
                 linea = archivo.ReadLine();
                 tabla.Add(temp);
             }
             archivo.Close();
         }
-        void Formatodgv()
-        {
-            dgv.Columns.Clear();
-            dgv.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 12);
-            dgv.Columns.Add("", "");
-            dgv.Rows.Add("Lexema"); dgv.Rows.Add("Id");dgv.Rows.Add("Nombre");
-            dgv.Refresh();
-        }
+        #endregion
         private void BtnAnalizar_Click(object sender, EventArgs e)
         {
             Formatodgv();
+            tokens.Clear();
             AnalizadorLexico(txtbox.Text + "$");
             AgregarCont();
-            if (reglas.Count != 0) AnalizadorSintactico();
-            else MessageBox.Show("No se puede realizar el analizis de sintaxis, sin los archivos de reglas y tabla");
-        }
-        void AnalizadorSintactico()
-        {
-
-            MostrarR();//muestra el resultado
-        }
-        void MostrarR()
-        {
-            if(valido)txtbox.BackColor = Color.LightGreen;
-            else txtbox.BackColor = Color.LightCoral;
-            valido = !valido;
-        }
-        void AgregarCont()
-        {
-            while(tokens.Count > 0){
-                token t = tokens.Dequeue();
-                int pos = dgv.Columns.Add("", "");
-                dgv[pos, 0].Value = t.Lexema;
-                dgv[pos, 1].Value = t.Id;
-                dgv[pos, 2].Value = t.Nombre;
+            if (BALE)           //bandera analizador lexico error
+            {
+                MessageBox.Show("Error en analisis lexico");
+                BALE = false;
             }
-            dgv.Refresh();
+            else if (reglas.Count != 0) AnalizadorSintactico();
+            else MessageBox.Show("No se puede realizar el analizis de sintactico, sin las tablas de reglas y transiciones");
         }
+        
+        #region Analizador Lexico       
         void AnalizadorLexico(string cadena)
         {
             string cad_act = "";
@@ -108,7 +115,7 @@ namespace WindowsFormsApp1
             while(act < cadena.Length)
             {
                 char c = cadena[act++];
-                cad_act += c;
+                cad_act += ""+c;
                 if (e == 0)
                 {
                     if (c == ' ' || c == '\r' || c == '\n') cad_act = "";
@@ -117,7 +124,7 @@ namespace WindowsFormsApp1
                     else if (EsNum(c)) e = 2;
                     else if (c == '-' || c == '+') EstadoFinal("OpSuma", ref cad_act, 14, ref e);
                     else if (c == '*' || c == '/') EstadoFinal("OpMultiplicacion", ref cad_act, 16, ref e);
-                    else if (cad_act == "||" || cad_act == "&&") EstadoFinal("OpLogico", ref cad_act, 15, ref e);
+                    else if (c == '|' || c == '&') e = 8;
                     else if (c == '=') e = 3;
                     else if (c == '<' || c == '>') e = 5;
                     else if (c == '!') e = 6;
@@ -194,10 +201,21 @@ namespace WindowsFormsApp1
                         EstadoFinal("Constante", ref cad_act, 17, ref e);
                     }
                 }
+                else if(e == 8)
+                {
+                    if (c == '|' || c == '&') EstadoFinal("OpLogico", ref cad_act, 15, ref e);
+                    else
+                    {
+                        act--;
+                        cad_act = cad_act.Substring(0, cad_act.Length - 1);
+                        EstadoFinal("Error", ref cad_act, -1, ref e);
+                    }
+                }
             }
         }
         void EstadoFinal(string nombre,ref string lexema, int id, ref int e)
         {
+            if (nombre == "Error") BALE = true;
             e = 0;
             tokens.Enqueue(new token(nombre, lexema, id));
             lexema = "";
@@ -216,21 +234,67 @@ namespace WindowsFormsApp1
         {
             return (x >= '0' && x <= '9');
         }
-        int StringtoInt(string s)
+        void AgregarCont()
         {
-            int t = 0, i = 0;
-            bool n = false;
-            if (s[0] == '-')
-            {
-                n = true;
-                ++i;
+            Queue<token> temp = new Queue<token>(tokens);
+            while(temp.Count > 0){
+                token t = temp.Dequeue();
+                int pos = dgv.Columns.Add("", "");
+                dgv[pos, 0].Value = t.Lexema;
+                dgv[pos, 1].Value = t.Id;
+                dgv[pos, 2].Value = t.Nombre;
             }
-            for(;i < s.Count();++i)
-            {
-                t *= 10;
-                t += s[i] - '0';
-            }
-            return n? -t : t;
+            dgv.Refresh();
         }
+        #endregion
+       
+        #region Analizador Sintactico
+        void AnalizadorSintactico()
+        {
+            Stack<int> pila = new Stack<int>();
+            pila.Push(0);
+            Stack<token> pila_tokens = new Stack<token>(tokens.Reverse());
+            int fila_act, valor_devuelto;
+            while(pila_tokens.Count > 0)
+            {
+                token token_act = pila_tokens.Peek();
+                fila_act = pila.Peek();
+                valor_devuelto = tabla[fila_act][token_act.Id+1];
+                if (valor_devuelto < 0)                             //reduccion
+                {
+                    if (valor_devuelto == -1) {                     //aceptada
+                        MostrarR(true);
+                        return;
+                    }
+                    ++valor_devuelto;                               //decremento la posicion
+                    int cant = reglas[-valor_devuelto].Y << 1;      //saco el lado derecho x2 de la pila
+                    while (cant-- > 0) pila.Pop();              
+                    pila_tokens.Push(new token("", "", reglas[-valor_devuelto].X));
+                }
+                else if (valor_devuelto > 0)                        //si cae aquí el token actual es descartado
+                {                                                   //y se agrega su id en la pila junto con el anterior
+                    pila_tokens.Pop();
+                    pila.Push(token_act.Id);
+                    pila.Push(valor_devuelto);
+                }
+                else break;                                         //no aceptada
+            }
+            MostrarR(false);
+            return;
+        }
+        void MostrarR(bool b)
+        {
+            if (b)
+            {
+                MessageBox.Show("Valido");
+                txtbox.BackColor = Color.LightGreen;
+            }
+            else
+            {
+                MessageBox.Show("Invalido");
+                txtbox.BackColor = Color.LightCoral;
+            }
+        }
+        #endregion
     }
 }
